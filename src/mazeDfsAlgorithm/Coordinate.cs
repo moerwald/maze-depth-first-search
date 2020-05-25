40457dead65368c9a8b5514d6d1883f92cce0950
moerwald <andreas.mewald@gmail.com>
2020-05-25 07:22:49 +0200

    Removed unused methods.

--- src/mazeDfsAlgorithm/Coordinate.cs
+++ src/mazeDfsAlgorithm/Coordinate.cs
@@ -3,16 +3,9 @@
     public struct Coordinate
     {
         public int X { get; set; }
+
         public int Y { get; set; }
-        //public bool Visited { get; set; }
-        //public bool Wall { get; set; }
 
-        public Coordinate UpperAdjacent() => new Coordinate { X = X + 1, Y = Y };
-        public Coordinate LowerAdjacent() => new Coordinate { X = X - 1, Y = Y };
-        public Coordinate LeftAdjacent() => new Coordinate { X = X , Y = Y - 1 };
-        public Coordinate RightAdjacent() => new Coordinate { X = X , Y = Y + 1 };
-
-        public override string ToString()
-            => $"X: {X}, Y: {Y}";
+        public override string ToString() => $"X: {X}, Y: {Y}";
     }
 }
