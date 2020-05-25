86ed1ab7ad2c451f004a33152688bdc35426940f
moerwald <andreas.mewald@gmail.com>
2020-05-25 07:20:57 +0200

    Renamed file. Added new testcases.

---
+++ src/mazeDfsAlgorithm.Tests/ThreeByThreeMazesTests.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+
+namespace mazeDfsAlgorithm.Tests
+{
+    public class ThreeByThreeMazesTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void MazeIsSolveable_Variant1()
+        {
+            var maze = new int [,]{
+                {0, 1 , 0, },
+                {0, 1 , 0, },
+                {0, 0 , 2, },
+                };
+
+            var result = SearchThroughMaze.Search(maze);
+
+            Assert.IsTrue(result[4].Equals(new Coordinate { X = 0, Y = 0 }));
+            Assert.IsTrue(result[3].Equals(new Coordinate { X = 1, Y = 0 }));
+            Assert.IsTrue(result[2].Equals(new Coordinate { X = 2, Y = 0 }));
+            Assert.IsTrue(result[1].Equals(new Coordinate { X = 2, Y = 1 }));
+            Assert.IsTrue(result[0].Equals(new Coordinate { X = 2, Y = 2 }));
+        }
+
+        [Test]
+        public void MazeIsSolveable_Variant2()
+        {
+            var maze = new int [,]{
+                {0, 0 , 0, },
+                {0, 1 , 0, },
+                {0, 1 , 2, },
+                };
+
+            var result = SearchThroughMaze.Search(maze);
+
+            Assert.IsTrue(result[4].Equals(new Coordinate { X = 0, Y = 0 }));
+            Assert.IsTrue(result[3].Equals(new Coordinate { X = 0, Y = 1 }));
+            Assert.IsTrue(result[2].Equals(new Coordinate { X = 0, Y = 2 }));
+            Assert.IsTrue(result[1].Equals(new Coordinate { X = 1, Y = 2 }));
+            Assert.IsTrue(result[0].Equals(new Coordinate { X = 2, Y = 2 }));
+        }
+
+        [Test]
+        public void MazeIsUnsolvable()
+        {
+            var maze = new int [,]{
+                {0, 1 , 0, },
+                {1, 1 , 0, },
+                {0, 1 , 2, },
+                };
+
+            var result = SearchThroughMaze.Search(maze);
+
+            Assert.IsTrue(result.Count == 0);
+        }
+    }
+}
