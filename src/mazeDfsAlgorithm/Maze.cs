40c96e5324e3a2c55474ad1cf4c945d4a6bc8156
moerwald <andreas.mewald@gmail.com>
2020-05-26 12:17:03 +0200

    Add new maze class to break responsibilities.

---
+++ src/mazeDfsAlgorithm/Maze.cs
@@ -0,0 +1,24 @@
+﻿namespace mazeDfsAlgorithm
+{
+    public class Maze
+    {
+        private readonly int[,] _maze;
+        private readonly int _mazeHight;
+        private readonly int _mazeWidth;
+
+        public Maze(int[,] maze)
+        {
+            _maze = maze;
+            _mazeHight = maze.GetLength(0);
+            _mazeWidth = maze.GetLength(1);
+        }
+
+        public bool CoordinatesOutsideOfMaze(int x, int y)
+        {
+            return (y < 0 || y >= _mazeWidth) ||
+                   (x < 0 || x >= _mazeHight);
+        }
+
+        public int GetValueAt(int x, int y) => _maze[x, y];
+    }
+}
