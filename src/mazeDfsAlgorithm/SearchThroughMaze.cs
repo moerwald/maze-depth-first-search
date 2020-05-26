45dc86f6aa943acfc407e0f7af3020ec4a1272a8
moerwald <andreas.mewald@gmail.com>
2020-05-26 12:08:18 +0200

    Removed unessary using statements.

--- src/mazeDfsAlgorithm/SearchThroughMaze.cs
+++ src/mazeDfsAlgorithm/SearchThroughMaze.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
 
 namespace mazeDfsAlgorithm
 {
