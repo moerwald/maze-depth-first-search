02167db4bc5e9d4afe83c691a3dac7fe7f0f35b0
moerwald <andreas.mewald@gmail.com>
2020-05-27 13:42:05 +0200

    Simple drawing version works.

--- src/MazeResolvingVisualizerConsole/Program.cs
+++ src/MazeResolvingVisualizerConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using mazeDfsAlgorithm;
+using System;
+using System.Threading;
 
 namespace MazeResolvingVisualizerConsole
 {
@@ -6,6 +8,37 @@ namespace MazeResolvingVisualizerConsole
     {
         static void Main(string[] args)
         {
+            var maze = new int[,]{
+                {0, 0 , 0, 0, 0},
+                {1, 1 , 1, 1, 0},
+                {0, 0 , 0, 0, 0},
+                {0, 1 , 1, 1, 1},
+                {0, 0 , 0, 0, 2},
+                };
+            var mazeObject = new Maze(maze);
+            var algo = new SearchThroughMaze(mazeObject, coord =>
+            {
+                mazeObject.SetValueAt(coord, 8);
+                RedrawMaze(mazeObject);
+            });
+            algo.Search();
+        }
+
+        private static void RedrawMaze(Maze mazeObject)
+        {
+            Console.Clear();
+
+            for (int y = 0; y < mazeObject.Height; y++)
+            {
+                for (int x = 0; x < mazeObject.Height; x++)
+                {
+                    Thread.Sleep(50);
+                    Console.Write($"{mazeObject.GetValueAt(new Coordinate() { X = x, Y = y })} ");
+                }
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(1000);
         }
     }
 }
