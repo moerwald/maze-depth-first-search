84d3ab1b1692d730372ff566ff846cd0a10810f8
moerwald <andreas.mewald@gmail.com>
2020-05-27 13:54:00 +0200

    Coloring code way found.

--- src/MazeResolvingVisualizerConsole/Program.cs
+++ src/MazeResolvingVisualizerConsole/Program.cs
@@ -10,10 +10,12 @@ namespace MazeResolvingVisualizerConsole
         {
             var maze = new int[,]{
                 {0, 0 , 0, 0, 0},
-                {1, 1 , 1, 1, 0},
-                {0, 0 , 0, 0, 0},
-                {0, 1 , 1, 1, 1},
-                {0, 0 , 0, 0, 2},
+                {1, 1 , 0, 1, 0},
+                {0, 1 , 0, 0, 0},
+                {0, 1 , 0, 1, 1},
+                {0, 1 , 0, 1, 0},
+                {1, 1 , 0, 0, 1},
+                {2, 0 , 0, 0, 0},
                 };
             var mazeObject = new Maze(maze);
             var algo = new SearchThroughMaze(mazeObject, coord =>
@@ -21,24 +23,41 @@ namespace MazeResolvingVisualizerConsole
                 mazeObject.SetValueAt(coord, 8);
                 RedrawMaze(mazeObject);
             });
-            algo.Search();
+            if (algo.Search().Count == 0)
+            {
+                Console.WriteLine("No way through maze found");
+            }
+
         }
 
         private static void RedrawMaze(Maze mazeObject)
         {
             Console.Clear();
+            var foreGroundColor = Console.ForegroundColor;
 
-            for (int y = 0; y < mazeObject.Height; y++)
+            for (int x = 0; x < mazeObject.Height; x++)
             {
-                for (int x = 0; x < mazeObject.Height; x++)
+                for (int y = 0; y < mazeObject.Width; y++)
                 {
-                    Thread.Sleep(50);
-                    Console.Write($"{mazeObject.GetValueAt(new Coordinate() { X = x, Y = y })} ");
+                    Thread.Sleep(10);
+
+                    var mazeValue = mazeObject.GetValueAt(new Coordinate() { X = x, Y = y });
+                    if (mazeValue == 8)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = foreGroundColor;
+                    }
+
+                    Console.Write($"{mazeValue} ");
                 }
                 Console.WriteLine();
             }
 
-            Thread.Sleep(1000);
+            Console.ForegroundColor = foreGroundColor;
+            Thread.Sleep(200);
         }
     }
 }
