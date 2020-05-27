9898902ab9766e83125bdcfbc927d6d1484ea3f7
moerwald <andreas.mewald@gmail.com>
2020-05-27 13:31:28 +0200

    Made Height and Width public

--- src/mazeDfsAlgorithm/Maze.cs
+++ src/mazeDfsAlgorithm/Maze.cs
@@ -5,25 +5,26 @@ namespace mazeDfsAlgorithm
     public class Maze
     {
         private readonly int[,] _maze;
-        private readonly int _mazeHight;
-        private readonly int _mazeWidth;
+        public int Height {get; private set;}
+        public int Width { get; private set; }
 
         public Maze(int[,] maze)
         {
             _maze = maze;
-            _mazeHight = maze.GetLength(0);
-            _mazeWidth = maze.GetLength(1);
+            Height = maze.GetLength(0);
+            Width = maze.GetLength(1);
         }
 
         public bool CoordinatesOutsideOfMaze(Coordinate coordinate)
         {
             var x = coordinate.X;
             var y = coordinate.Y;
-            return (y < 0 || y >= _mazeWidth) ||
-                   (x < 0 || x >= _mazeHight);
+            return (y < 0 || y >= Width) ||
+                   (x < 0 || x >= Height);
         }
 
         public int GetValueAt(Coordinate coordinate) => _maze[coordinate.X, coordinate.Y];
+        public void SetValueAt(Coordinate coordinate, int newValue) => _maze[coordinate.X, coordinate.Y] = newValue;
 
         internal bool IsExit(Coordinate newCord) => GetValueAt(newCord) == 2;
 
